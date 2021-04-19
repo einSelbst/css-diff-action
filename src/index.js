b882de290ba66038e2b06c2b980cb46556af5a6e
einSelbst <silvio@mailbox.org>
2021-04-20 00:32:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,40 +6,25 @@ const github = require('@actions/github')
 const { createCommentMarkdown } = require('./create-comment')
 
 async function run() {
-	
-	const patterns = ['**/css', '**/*.css']
-	const globber = await glob.create(patterns.join('\n'))
-	
+	// find the css file in the project
+	//const patterns = ['**/css', '**/*.css']
+	//const globber = await glob.create(patterns.join('\n'))
+	const globber = await glob.create('./.next/static/css/*.css', { followSymbolicLinks: false })
 	//const globber = await glob.create(core.getInput('path'), { followSymbolicLinks: false })
 	const files = await globber.glob()
-	console.log(`Found ${files.length} files to upload`)
-	console.log(`First file is ${files[0]}`)
+
 	//const cssPath = core.getInput('css-path')
-	//console.log(cssPath)
-	echo "::debug::Set the Octocat variable"
-	core.debug("test")
-	//core.debug(cssPath)
-
 	const webhookToken = core.getInput('project-wallace-token')
-	console.log(webhookToken)
-
 	const githubToken = core.getInput('github-token')
-	console.log(githubToken)
-
 	const shouldPostPrComment = core.getInput('post-pr-comment') === 'true'
 	const { eventName, payload } = github.context
-	console.log(eventName)
-
 	//if (eventName !== 'pull_request') {
 	//	console.log("finish early")
 	//	return
 	//}
 
 	// Read CSS file
-	console.log("read css file")
 	//var cssFiles = fs.readdirSync(cssPath).filter(fn => fn.endsWith('.css'));
-	//console.log(cssFiles)
-	//console.log(cssFiles[0])
 	try {
 		const css = fs.readFileSync(files[0], 'utf8')
 
